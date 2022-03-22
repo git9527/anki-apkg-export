e3c3d6c88f19b622c7326ca0e105389595188a5e
git9527 <398816613@qq.com>
2022-03-22 18:42:55 +0800

    update default front

--- src/template.js
+++ src/template.js
@@ -1,5 +1,5 @@
 export default function createTemplate({
-  questionFormat = '请转换笔记模板后使用{{id}}',
+  questionFormat = '请先导入【示例牌组-商业版】{{id}}',
   answerFormat = '{{FrontSide}}\n\n<hr id="answer">\n\n{{question}}',
   css = '.card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\nbackground-color: white;\n}\n'
 } = {}) {
